47c98e0587d10c9b84b13c416768b91e8c978d3e
AjayM-adelphi <ajaymohan@mail.adelphi.edu>
2026-04-30 21:33:43 -0400

    Done

--- assignments/week_08/integration.py
+++ assignments/week_08/integration.py
@@ -2,14 +2,29 @@
 # this module will contain functions for numerical integration
 
 
-def trapezoidal_rule():
-    #write the arguments, docstring, and actual code of this function
 
-    return
+def trapezoidal_rule(f, a, b, N):
+    h = (b - a) / N
+    s = 0.5 * (f(a) + f(b))
+    
+    for i in range(1, N):
+        s += f(a + i*h)
+    
+    return h * s
 
 
-
-def simpsons_rule():
-    #write the arguments, docstring, and actual code of this function
-
-    return
+def simpsons_rule(f, a, b, N):
+    if N % 2 != 0:
+        raise ValueError("N must be even for Simpson's rule")
+    
+    h = (b - a) / N
+    s = f(a) + f(b)
+    
+    for i in range(1, N):
+        x = a + i*h
+        if i % 2 == 0:
+            s += 2 * f(x)
+        else:
+            s += 4 * f(x)
+    
+    return (h / 3) * s
